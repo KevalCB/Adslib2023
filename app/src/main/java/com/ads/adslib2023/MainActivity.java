4ac606e7452eb854ef0ec4abc8a874a9bfadd6bc
KevalCB <keval.codebyte@gmail.com>
2023-06-16 12:00:57 +0530

    Initial commit

---
+++ app/src/main/java/com/ads/adslib2023/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ads.adslib2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ads.ads2023.Ads_Preference;
+import com.ads.ads2023.VPN;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
